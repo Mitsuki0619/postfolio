c7805e7420cadcf862d8812141036c07eeb0e9f9
MitsukiIwamura <boc.fsfb@gmail.com>
2022-12-04 04:58:15 +0900

    feat:課題内容に該当する設定とライブラリのインストール

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+// pages/_document.js
+
+import { ColorModeScript, theme } from "@chakra-ui/react";
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Noto+Sans+JP"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
