0e833ece91e75036459e35c3d7904b470a837120
MitsukiIwamura <boc.fsfb@gmail.com>
2023-01-08 14:16:41 +0900

    build: relay削除、urql導入、codegen設定

---
+++ codegen.ts
@@ -0,0 +1,21 @@
+import { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "./apps/server/graphql/schema/schema.graphql",
+  // documents: "./apps/client/src/**/*.tsx",
+  generates: {
+    "./apps/server/graphql/__generated__/graphql.ts": {
+      plugins: ["typescript", "typescript-resolvers"],
+    },
+    "./apps/server/graphql.schema.json": {
+      plugins: ["introspection"],
+    },
+    "./apps/client/src/graphql/__generated__/graphql-operations.ts": {
+      plugins: ['typescript', 'typescript-operations', 'typed-document-node'],
+      preset: 'client',
+    }
+  },
+};
+
+export default config;
